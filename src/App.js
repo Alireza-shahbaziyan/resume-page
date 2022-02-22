fc169f6adea5f49294e3f04a7f56cb025ee2a1d2
A & A <91082232+aarepos@users.noreply.github.com>
2022-02-22 16:53:24 +0330

    Add Skills

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+// const back_end = document.getElementById("Back-end");
+// back_end.onclick()
+
+const backEndFunc = () => {
+    document.getElementById("body").innerHTML = <div class="alert alert-warning" role="alert">
+        This is a warning alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+    </div>
+}
